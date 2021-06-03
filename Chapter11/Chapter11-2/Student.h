25e9db65330587298d16b24e3bb2bb3fe3069a78
hyezz <hyezzx@gmail.com>
2021-06-03 23:37:38 +0900

    Ch11.2.상속의 기본 (2)

---
+++ Chapter11/Chapter11-2/Student.h
@@ -0,0 +1,39 @@
+#pragma once
+
+//#include <iostream>
+//#include <string>
+#include "Person.h"
+
+class Student : public Person
+{
+private:
+	//std::string m_name;
+	int m_intel; // intelligence
+
+public:
+	Student(const std::string& name_in = "No Name", const int& intel_in = 0)
+		//: m_name(name_in), m_intel(intel_i)
+		: Person(name_in), m_intel(intel_in)
+	{}
+
+	void setIntel(const int& intel_in)
+	{
+		m_intel = intel_in;
+	}
+
+	int getIntel() const
+	{
+		return m_intel;
+	}
+
+	void study()
+	{
+		std::cout << getName() << " is studying " << std::endl;
+	}
+
+	friend std::ostream& operator << (std::ostream& out, const Student& student)
+	{
+		out << student.getName() << " " << student.getIntel(); 
+		return out;
+	}
+};
