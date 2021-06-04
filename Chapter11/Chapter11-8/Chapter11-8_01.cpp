ff037e5c65b85aad086fd045447e1d9d242eed9d
hyezz <hyezzx@gmail.com>
2021-06-05 00:17:57 +0900

    Ch11.8.상속 받은 함수를 감추기

---
+++ Chapter11/Chapter11-8/Chapter11-8_01.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+class Base
+{
+protected:
+	int m_i;
+
+public:
+	Base(int value)
+		: m_i(value)
+	{}
+
+	void print()
+	{
+		cout << "I'm base " << endl;
+	}
+};
+
+class Derived : public Base
+{
+private:
+	double m_d;
+
+public:
+	Derived(int value)
+		: Base(value)
+	{}
+
+	// Derived 클래스에서 사용 가능하게
+	using Base::m_i;
+
+	// Derived 클래스에서 사용 불가능하게
+private:
+	using Base::print;	// do not add () : 함수 이름만 있으면 된다
+	void print() = delete;
+};
+
+int main()
+{
+	Base base(5);
+	base.print();
+
+	Derived derived(7);
+
+	derived.m_i = 1024;
+	//derived.print();
+
+	return 0;
+}
