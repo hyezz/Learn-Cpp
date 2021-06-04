5f982ea4298bc1e23adc96ade4c742ddc3c86635
hyezz <hyezzx@gmail.com>
2021-06-05 00:40:12 +0900

    Ch11.9.다중 상속

---
+++ Chapter11/Chapter11-9/Chapter11-9_01.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+class USBDevice
+{
+private:
+	long m_id;
+
+public:
+	USBDevice(long id) : m_id(id) {}
+
+	long getID() { return m_id; }
+
+	void plugAndPlay() {}
+};
+
+class NetworkDevice
+{
+private:
+	long m_id;
+
+public:
+	NetworkDevice(long id) : m_id(id) {}
+
+	long getID() { return m_id; }
+
+	void networking() {}
+};
+
+class USBNetworkDevice : public USBDevice, public NetworkDevice
+{
+public:
+	USBNetworkDevice(long usb_id, long net_id)
+		: USBDevice(usb_id), NetworkDevice(net_id)
+	{}
+};
+
+int main()
+{
+	USBNetworkDevice my_device(3.14, 6.022);
+
+	my_device.networking();
+	my_device.plugAndPlay();
+
+	// 상속받은 클래스에 이름이 같은 함수가 있을 때 어떤 클래스의 함수인지 특정해야 함
+	my_device.USBDevice::getID();
+	my_device.NetworkDevice::getID();
+
+	return 0;
+}
