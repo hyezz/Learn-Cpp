560aa78059f7c595bcc1d54485a164244588fda6
hyezz <hyezzx@gmail.com>
2021-06-06 22:50:16 +0900

    Ch12.1.다형성의 기본 개념

---
+++ Chapter12/Chapter12-1/Chapter12-1_01.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animal
+{
+protected:
+	string m_name;
+
+public:
+	Animal(string name)
+		: m_name(name)
+	{}
+
+public:
+	string getName()
+	{
+		return m_name;
+	}
+
+	// super class member function을 virtual로 구현하면 sub class member function을 잘 사용할 수 있다
+	virtual void speak() const
+	{
+		cout << m_name << " ??? " << endl;
+	}
+};
+
+class Cat : public Animal
+{
+public:
+	Cat(string name)
+		: Animal(name)
+	{}
+
+	void speak() const
+	{
+		cout << m_name << " Meow " << endl;
+	}
+};
+
+class Dog : public Animal
+{
+public:
+	Dog(string name)
+		: Animal(name)
+	{}
+
+	void speak() const
+	{
+		cout << m_name << " Woof " << endl;
+	}
+};
+
+int main()
+{
+	Animal animal("my animal");
+	Cat cat("my cat");
+	Dog dog("my dog");
+
+	animal.speak();
+	cat.speak();
+	dog.speak();
+
+	Animal* ptr_animal1 = &cat;
+	Animal* ptr_animal2 = &dog;
+
+	ptr_animal1->speak();
+	ptr_animal2->speak();
+
+	Cat cats[] = { Cat("cat1"), Cat("cat2"), Cat("cat3"), Cat("cat4"), Cat("cat5") };
+	Dog dogs[] = { Dog("dog1"), Dog("dog2") };
+
+	for (int i = 0; i < 5; ++i)
+		cats[i].speak();
+
+	for (int i = 0; i < 2; ++i)
+		dogs[i].speak();
+
+	// sub class의 객체를 super class의 포인터에 넣어서 사용 (다형성)
+	Animal* my_animals[] = { &cats[0], &cats[1], &cats[2], &cats[3], &cats[4],
+							 &dogs[0], &dogs[1] };
+	
+	for (int i = 0; i < 7; ++i)
+		my_animals[i]->speak();
+
+	return 0;
+}
